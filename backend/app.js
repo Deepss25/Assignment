5f5a5c71b0c34160e1b03b8ea382bb9df614fdea
deepu <deepikakamathan6@gmail.com>
2025-07-26 10:36:50 +0530

    Added root route to fix Cannot GET /

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,17 @@ const loanRoutes = require('./routes/loans');
 const customerRoutes = require('./routes/customers');
 const paymentRoutes = require('./routes/payments'); // 👈 correct path
 
-
-app.use(cors({origin: 'http://localhost:8080'}));
+app.use(cors({ origin: 'http://localhost:8080' }));
 app.use(express.json());
 
 app.use('/api/loans', loanRoutes);
 app.use('/customers', customerRoutes);
 app.use('/api/payments', paymentRoutes);
 
+// 👇 Add this route to avoid "Cannot GET /"
+app.get('/', (req, res) => {
+  res.send('Bank Lending System Backend is running');
+});
 
 app.listen(5000, () => {
   console.log('Backend server running on port 5000');
